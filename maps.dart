39ef76bc9e04f1625bca18ae9eb02fb58f185e74
Paulo Roberto <evans_sp@hotmail.com>
2020-03-18 18:12:31 -0300

    Maps

---
+++ maps.dart
@@ -0,0 +1,16 @@
+main(){
+  //Criando uma maps para acessar dados
+  var maps = {
+    "nome" :  "Paulo Roberto",
+    "idade" : 27,
+    "cidade" : "Maringa",
+    "estado" : "Parana"
+  };
+
+  //Imprimindo dados do maps
+  print(maps["nome"]);
+  print(maps["idade"]);
+
+  //Maps é uma forma de representar dados
+
+}
