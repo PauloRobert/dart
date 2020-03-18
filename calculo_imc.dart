9cc384ae6817ec3a1322698f26ffd07708d0dfe2
Paulo Roberto <evans_sp@hotmail.com>
2020-03-18 18:02:09 -0300

    modulo de funçoes

--- calculo_imc.dart
+++ calculo_imc.dart
@@ -16,25 +16,30 @@ calculoImc() {
   String textAltura = stdin.readLineSync();
   double altura = double.parse(textAltura);
 
-  double calcimc = peso / (altura * altura);
+  double imc = calcImc(peso, altura);
 
-  imprimirResultados(calcimc);
+  imprimirResultados(imc);
+}
+
+//Função que faz o calculo
+double calcImc(int peso, double altura) {
+  return peso / (altura * altura);
 }
 
 //Criando a segunda função
 //Passando o calculo como parâmetro
 
-imprimirResultados(double calcimc) {
+imprimirResultados(double imc) {
   print("====  ====");
-  if (calcimc < 18.5) {
+  if (imc < 18.5) {
     print("Abaixo do peso");
-  } else if (calcimc > 18.5 && calcimc < 24.9) {
+  } else if (imc > 18.5 && imc < 24.9) {
     print("Peso normal");
-  } else if (calcimc > 25 && calcimc < 29.9) {
+  } else if (imc > 25 && imc < 29.9) {
     print("Sbrepeso");
-  } else if (calcimc > 30 && calcimc < 34.9) {
+  } else if (imc > 30 && imc < 34.9) {
     print("Obesidade grau 1");
-  } else if (calcimc > 35 && calcimc < 39.9) {
+  } else if (imc > 35 && imc < 39.9) {
     print("Obesidade grau 2");
   } else {
     print("obesidade grau 3");
